761bd0d7593fee6fd2fdd656806cbd99ecc52075
mr.su <1994@suyafei.com>
2017-03-21 23:13:17 +0800

    fix dev shell

--- wechatclient/shell/gitrobot.py
+++ wechatclient/shell/gitrobot.py
@@ -1,27 +1,27 @@
 # -*- coding:utf-8 -*-
 import os
 import time
+
+
 def dev_robot():
-	path = "/home/pi/code/wechatserver/wechatclient/"
-	os.chdir(path)
-	try:
-		val = os.system("git commit -am'gitrobot'")
-		val = os.system("git pull")
-	except Exception as e:
-		raise
-	try:
-		os.system("python clinet.py")
-	except Exception as e:
-		raise e
+    path = "/home/pi/code/wechatserver/wechatclient/"
+    os.chdir(path)
+    try:
+        os.system("git fetch  --all")
+        os.system("git reset --hard tornado")
+        os.system("git pull")
+    except Exception as e:
+        raise
 
-
+    try:
+        os.system("python clinet.py")
+    except Exception as e:
+        raise e
 
 
 if __name__ == '__main__':
 
-	
-	while True:
-		time.sleep(100)
-		cas_val = dev_robot()
-		print cas_val
-	
+    while True:
+        time.sleep(100)
+        cas_val = dev_robot()
+        print cas_val
