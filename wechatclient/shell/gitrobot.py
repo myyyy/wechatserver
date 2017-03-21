4bc7cb93baec9d7ecf5ba7bc23b53e4fd51b489c
mr.su <1994@suyafei.com>
2017-03-21 23:10:45 +0800

    Signed-off-by: mr.su <1994@suyafei.com>

--- wechatclient/shell/gitrobot.py
+++ wechatclient/shell/gitrobot.py
@@ -4,16 +4,24 @@ import time
 def dev_robot():
 	path = "/home/pi/code/wechatserver/wechatclient/"
 	os.chdir(path)
-	val = os.system("git commit -am'gitrobot'")
-	val = os.system("git pull")
-	os.system("python clinet.py")
+	try:
+		val = os.system("git commit -am'gitrobot'")
+		val = os.system("git pull")
+	except Exception as e:
+		raise
+	try:
+		os.system("python clinet.py")
+	except Exception as e:
+		raise e
+
+
 
 
 if __name__ == '__main__':
 
 	
 	while True:
-		time.sleep(100) 
+		time.sleep(100)
 		cas_val = dev_robot()
 		print cas_val
 	
