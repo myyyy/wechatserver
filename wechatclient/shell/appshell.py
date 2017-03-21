6db52706e7750edb09be08bc2e54153fda085d70
mr.su <1994@suyafei.com>
2017-03-22 00:12:54 +0800

    Signed-off-by: mr.su <1994@suyafei.com>

---
+++ wechatclient/shell/appshell.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+import os
+import time
+
+
+def app_timer():
+	path = "/home/pi/code/wechatserver/wechatclient/"
+    os.chdir(path)
+	while True:
+		time.sleep(10)
+		try:
+			os.system("python clinet.py")
+		except Exception as e:
+		        pass
+
+
+if __name__ == '__main__':
+	app_timer()
