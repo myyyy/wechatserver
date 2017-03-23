feb580cac97e31a50cae4cdfbf77c340803e2b0d
mrsu <1994@suyafei.com>
2017-03-23 23:52:18 +0800

    fix bug

--- wechatclient/clinet.py
+++ wechatclient/clinet.py
@@ -44,12 +44,14 @@ class IndexHandler(tornado.web.RequestHandler):
         msg = parse_message(xml)
         if msg.content in 'status':
             data = Machine().fast_data
+            reply = TextReply(content=data, message=msg)
             reply = create_reply(data, message=msg)
             _reply = reply.render()
             self.write(_reply)
         else:
             robot = TuLingRobot(msg.content)
-            reply = create_reply(datarobot.reply, message=msg)
+            reply = create_reply(robot.reply, message=msg)
+            print (reply)
             _reply = reply.render()
             self.write(_reply)
 
