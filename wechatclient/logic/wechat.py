d98e20fd7f94d7377dc53645531ecfaf526158bf
mrsu <1994@suyafei.com>
2017-03-26 18:39:19 +0800

    .

--- wechatclient/logic/wechat.py
+++ wechatclient/logic/wechat.py
@@ -8,44 +8,43 @@ SECRET = 'fa32576b9daa6fd020c0104e6092196a'
 
 class BaseHandler(tornado.web.RequestHandler):
 
-    def __init__(self, arg):
+    def __init__(self, application, request, **kwargs):
         self.client = self.get_client()
 
     def get_client():
-   	client = WeChatClient(APPID, SECRET)
+        client = WeChatClient(APPID, SECRET)
         client.menu.add_conditional({
-			    "button":[
-			        {
-			            "type":"click",
-			            "name":"阅读",
-			            "key":"V1001_TODAY_READ"
-			        },
-			        {
-			            "type":"click",
-			            "name":"音乐",
-			            "key":"V1001_TODAY_MUSIC"
-			        },
-			        {
-			            "name":"时光",
-			            "sub_button":[
-			                {
-			                    "type":"view",
-			                    "name":"故事",
-			                    "url":"http://www.soso.com/"
-			                },
-			                {
-			                    "type":"view",
-			                    "name":"流年",
-			                    "url":"http://v.qq.com/"
-			                },
-			                {
-			                    "type":"click",
-			                    "name":"关于我们",
-			                    "key":"ABOUT_US"
-			                }
-			            ]
-			        }
-			    ],
-			})
+            "button": [
+                {
+                                    "type": "click",
+                                    "name": "阅读",
+                                    "key": "V1001_TODAY_READ"
+                                    },
+                {
+                    "type": "click",
+                    "name": "音乐",
+                    "key": "V1001_TODAY_MUSIC"
+                },
+                {
+                    "name": "时光",
+                    "sub_button": [
+                        {
+                            "type": "view",
+                            "name": "故事",
+                            "url": "http://www.soso.com/"
+                        },
+                        {
+                            "type": "view",
+                            "name": "流年",
+                            "url": "http://v.qq.com/"
+                        },
+                        {
+                            "type": "click",
+                            "name": "关于我们",
+                            "key": "ABOUT_US"
+                        }
+                    ]
+                }
+            ],
+        })
         return client
-
